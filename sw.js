dc0d49cdc269d46711349015873325046e3d236e
vishalkhari965-cpu <vishalkhari965@gmail.com>
2026-08-21 09:13:03 +0530

    Refresh installed Ludo app updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'ludo-studio-v2';
+const CACHE = 'ludo-studio-v3';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
